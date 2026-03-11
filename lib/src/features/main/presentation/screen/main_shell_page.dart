f20d321e7be08e4502c6510d54b75a18e16a250e
wndnjs00 <juwonjeon852@gmail.com>
2026-03-11 16:17:47 +0900

    [feat/#1] bottomNavigation 제거후, MainScreen만 보이도록 수정 (추후 확장하기 좋은구조로 구현)

--- lib/src/features/main/presentation/screen/main_shell_page.dart
+++ lib/src/features/main/presentation/screen/main_shell_page.dart
@@ -1,38 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'main_screen.dart';
-import 'ai_helper_screen.dart';
+/*import 'ai_helper_screen.dart';*/
 
 class MainShellPage extends StatelessWidget {
   const MainShellPage({super.key, required this.child});
 
   final Widget child;
 
-  int _calculateSelectedIndex(BuildContext context) {
-    return 0;
-
-    final location = GoRouterState.of(context).uri.toString();
-    if (location.startsWith(AiHelperScreen.routePath)) {
-      return 1;
-    }
-    return 0;
-  }
-
-  void _onItemTapped(BuildContext context, int index) {
-    switch (index) {
-      case 0:
-        context.go(MainScreen.routePath);
-        break;
-      case 1:
-        context.go(AiHelperScreen.routePath);
-        break;
-    }
-  }
+  /* bottomNavigationBar 추가시, 주석 해제 */
+  // int _calculateSelectedIndex(BuildContext context) {
+  //   final location = GoRouterState.of(context).uri.toString();
+  //   if (location.startsWith(AiHelperScreen.routePath)) {
+  //     return 1;
+  //   }
+  //   return 0;
+  // }
+  //
+  // void _onItemTapped(BuildContext context, int index) {
+  //   switch (index) {
+  //     case 0:
+  //       context.go(MainScreen.routePath);
+  //       break;
+  //     case 1:
+  //       context.go(AiHelperScreen.routePath);
+  //       break;
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
-    final currentIndex = _calculateSelectedIndex(context);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('WaterLog'),
@@ -48,20 +45,22 @@ class MainShellPage extends StatelessWidget {
         ],
       ),
       body: child,
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: currentIndex,
-        onDestinationSelected: (index) => _onItemTapped(context, index),
-        destinations: const [
-          NavigationDestination(
-            icon: Icon(Icons.home),
-            label: '물마시기',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.star),
-            label: 'Ai도우미',
-          ),
-        ],
-      ),
+
+      /* bottomNavigationBar 추가시, 주석 해제 */
+      // bottomNavigationBar: NavigationBar(
+      //   selectedIndex: _calculateSelectedIndex(context),
+      //   onDestinationSelected: (index) => _onItemTapped(context, index),
+      //   destinations: const [
+      //     NavigationDestination(
+      //       icon: Icon(Icons.home),
+      //       label: '물마시기',
+      //     ),
+      //     NavigationDestination(
+      //       icon: Icon(Icons.star),
+      //       label: 'Ai도우미',
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
