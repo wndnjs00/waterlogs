1d4f69c8f2c047122138339c01237a718acb2b81
wndnjs00 <juwonjeon852@gmail.com>
2026-03-11 20:32:08 +0900

    [feat/#3] firebase 연결, 소셜로그인 이미지 첨부

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:waterlogs/firebase_options.dart';
 
 import 'src/core/router/app_router.dart';
 import 'src/core/theme/app_theme.dart';
 
-void main() {
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     const ProviderScope(
       child: WaterLogsApp(),
--- lib/src/core/util/asset_path.dart
+++ lib/src/core/util/asset_path.dart
@@ -3,4 +3,9 @@ class AssetPath {
   static const badgeIcon = "assets/badge_img.png";
   static const logoutIcon = "assets/logout_img.png";
   static const signoutIcon = "assets/signout_img.png";
+
+  // login
+  static const naverLoginIcon = "assets/ic_naver_logo.png";
+  static const kakaoLoginIcon = "assets/kakao_login_large_wide.png";
+  static const googleLoginIcon = "assets/ic_google_logo.png";
 }
